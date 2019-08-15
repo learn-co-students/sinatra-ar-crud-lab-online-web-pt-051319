1ee9f1eb3c479f3b51efba7d862c7d8cb3b8de12
Kelley Chaplain <kmchaplain@gmail.com>
2019-08-14 22:49:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles/new' do
+    @article = Article.new
     erb :new
+  end
+
+  post '/articles' do
+    # binding.pry
+    @article = Article.create(params)
+    erb :show
+    redirect '/articles/#{@article.id}'
   end  
+  
+  get '/articles' do
+    @article = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end  
+  
+  
 end
